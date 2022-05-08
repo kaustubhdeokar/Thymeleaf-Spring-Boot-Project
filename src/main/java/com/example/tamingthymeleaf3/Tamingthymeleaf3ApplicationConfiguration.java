716630e3427197cee6809354212afd1c01ce9144
kaustubhdeokar <kaustubhd9@gmail.com>
2022-05-08 18:09:41 +0530

    fragments added

--- src/main/java/com/example/tamingthymeleaf3/Tamingthymeleaf3ApplicationConfiguration.java
+++ src/main/java/com/example/tamingthymeleaf3/Tamingthymeleaf3ApplicationConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.tamingthymeleaf3;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
+@Configuration
 public class Tamingthymeleaf3ApplicationConfiguration {
     @Bean
     public ITemplateResolver svgTemplateResolver() {
